04e52ac439c1069d1f7f3c07a7aaade83fa369d2
Yashank Singh <yashank94@gmail.com>
2022-12-02 11:37:50 +0530

    Configured logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from abc import ABC
 
 import discord
@@ -8,16 +9,16 @@ from dotenv import load_dotenv
 from utilities import select_info_view, Options
 
 
-class MyBot(discord.Bot, ABC):
-    def __init__(self):
-        intents = discord.Intents.all()
-        super().__init__(command_prefix='?', intents=intents)
+logging.basicConfig(filename='logs/axonavi.log',
+                    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+                    level=logging.INFO,
+                    filemode='w',
+                    encoding='utf-8')
+logging_handler = logging.StreamHandler()
+logger = logging.getLogger('discord')
+logger.addHandler(logging_handler)
 
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-        activity = discord.Activity(type=3, name="you poop... always!")
-        await self.change_presence(status=discord.Status.dnd, activity=activity)
+logger.info("Loaded Logger")
 
 
 load_dotenv()
@@ -30,12 +31,28 @@ BOXLOTL_GUILD_ID = os.getenv('BOXLOTL_GUILD_ID')
 MY_GUILD = discord.Object(MY_GUILD_ID)
 PRIV_GUILD = discord.Object(PRIV_GUILD_ID)
 
+logger.info("Loaded environment variables")
+
+
+class MyBot(discord.Bot, ABC):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(command_prefix='?', intents=intents)
+
+    async def on_ready(self):
+        logger.info(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+        activity = discord.Activity(type=3, name="you poop... always!")
+        await self.change_presence(status=discord.Status.dnd, activity=activity)
+
+
 bot = MyBot()
 
 
 @bot.slash_command(name="status", description="Set status of bot!", guild_ids=[PRIV_GUILD_ID])
 @option("status", discord.Status)
-async def set_status(ctx, status: discord.Status):
+async def set_status(ctx: discord.ApplicationContext, status: discord.Status):
+    logger.info(f"{ctx.user} used /status {status}")
     if ctx.user.id != 520923251367608322:
         return
     if status == "offline" or status == "streaming":
@@ -47,12 +64,13 @@ async def set_status(ctx, status: discord.Status):
     await bot.change_presence(status=status, activity=prev_activity)
     await ctx.respond(f"Changed status to: **{status}**",
                       ephemeral=True)
-    print(f"{ctx.user} changed bot status to {status}")
+    logger.info(f"{ctx.user} changed bot status to {status}")
 
 
 @bot.slash_command(name="activity", description="Set activity of bot!", guild_ids=[PRIV_GUILD_ID])
 @option("activity_type", str, choices=["playing", "listening", "watching"])
-async def set_activity(ctx, activity_type: str, *, activity_message: str):
+async def set_activity(ctx: discord.ApplicationContext, activity_type: str, *, activity_message: str):
+    logger.info(f"{ctx.user} used /activity {activity_type} {activity_message}")
     if ctx.user.id != 520923251367608322:
         return
     if activity_message == "streaming":
@@ -73,7 +91,7 @@ async def set_activity(ctx, activity_type: str, *, activity_message: str):
                               status=prev_status)
     await ctx.respond(f"Changed activity to: **{activity_type}** {activity_message}",
                       ephemeral=True)
-    print(f"{ctx.user} changed bot activity to {activity_type} {activity_message}")
+    logger.info(f"{ctx.user} changed bot activity to {activity_type} {activity_message}")
 
 
 async def get_choices(ctx: discord.AutocompleteContext):
@@ -85,19 +103,22 @@ async def get_choices(ctx: discord.AutocompleteContext):
 @bot.command(name="info", description="Get info about things in-game!")
 @option("info_option", str, description="Select an option you need info about!", choices=Options.main)
 @option("info_type", str, description="Select what you need info about!", autocomplete=get_choices)
-async def game_info(ctx, info_option: str, info_type: str = "none"):
+async def game_info(ctx: discord.ApplicationContext, info_option: str, info_type: str = "none"):
+    logger.info(f"{ctx.user} used /info {info_option} {info_type}")
     await ctx.respond(f"Hey {ctx.user}!")
     await select_info_view(ctx, info_option, info_type)
 
 
 @bot.command(name="idkwtnt", guild_ids=[PRIV_GUILD_ID, BOXLOTL_GUILD_ID])
-async def poop(ctx, anything: str):
+async def poop(ctx: discord.ApplicationContext, anything: str):
+    logger.info(f"{ctx.user} used /idkwtnt {anything}")
     if ctx.user.id not in [520923251367608322, 801290250881335296]:
         await ctx.respond("You cannot use this you little sh*t",
                           ephemeral=True)
         return
     await ctx.send(anything)
     await ctx.respond("Message sent", ephemeral=True)
+    logger.info(f"{ctx.user} sent \"{anything}\" in {ctx.channel}")
 
 
 @bot.listen('on_message')
