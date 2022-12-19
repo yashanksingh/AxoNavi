2de97fa908e0046ed3b6290cdbe21e34e425bb0d
Yashank Singh <yashank94@gmail.com>
2022-12-19 22:32:23 +0530

    Whitespaces

--- utilities.py
+++ utilities.py
@@ -8,6 +8,7 @@ class Options:
     main = ["Help", "NPCs", "Mobs and Bosses", "Items"]
     boss = ["Spider Queen", "Cuboid", "Grave Digger", "Demon", "Crypt Slasher", "Elder Slasher", "Magmatic Spirit", "Royal Gargoyle"]
 
+
 async def boss_spawn_alert(boss, channel):
     main_embed = Embed(
         # title=boss,
